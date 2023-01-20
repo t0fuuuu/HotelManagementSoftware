3bf50861f971515a4bb8337256f47417076f2a0f
Axtence <jadenang10@yahoo.com.sg>
2023-01-20 21:41:57 +0800

    Now I think all classes are finish. Check pls

--- HotelManagementSoftware/Stay.cs
+++ HotelManagementSoftware/Stay.cs
@@ -8,22 +8,32 @@ namespace HotelManagementSoftware
 {
     class Stay
     {
-        public DateTime CheckinDate { get; set; }
+        public DateTime CheckInDate { get; set; }
         public DateTime CheckOutDate { get; set; }
         public List<Room> RoomList { get; set; } = new List<Room>();
         public Stay() { }
         public Stay(DateTime CID, DateTime COD)
         {
-            CheckinDate = CID;
+            CheckInDate = CID;
             CheckOutDate = COD;
         }
-        public AddRoom(Room r)
+        public void AddRoom(Room r)
         {
             RoomList.Add(r);
         }
         public double CalculateTotal()
         {
-
+            int days = CheckOutDate.Subtract(CheckInDate).Days;
+            double total = 0;
+            foreach(Room r in RoomList) 
+            {
+                total += days * r.CalculateCharges();
+            }
+            return total;
+        }
+        public override string ToString()
+        {
+            return "Check In Date: " + CheckInDate + " Check Out Date: " + CheckOutDate;
         }
     }
 }
