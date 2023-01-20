290481e805c53ff2e5eb5d8a5efbd18e2625b427
ryanm <ryanm@LAPTOP-6K3IIERN>
2023-01-20 17:47:49 +0800

    added abstract class stay

---
+++ HotelManagementSoftware/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSoftware
+{
+    abstract class Room
+    {
+        public int RoomNumber { get; set; }
+        public string? BedConfiguration { get; set; }
+        public double DailyRate { get; set; }
+        public bool IsAvail { get; set; }
+        public Room() { }
+        public Room(int r, string bedcfg,double dr, bool isavl)
+        {
+            RoomNumber = r;
+            BedConfiguration = bedcfg;
+            DailyRate = dr;
+            IsAvail = isavl;
+        }
+        public abstract double CalculateCharges();
+        public override string ToString()
+        {
+            return "Room Number: " + RoomNumber + "Bed Configuration: " + BedConfiguration + "Daily Rate: " + DailyRate + "IsAvailable: " + IsAvail;
+        }
+    }
+}
---
+++ HotelManagementSoftware/Stay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSoftware
+{
+    class Stay
+    {
+        public DateTime CheckinDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public List<Room> RoomList { get; set; } = new List<Room>();
+        public Stay() { }
+        public Stay(DateTime CID, DateTime COD)
+        {
+            CheckinDate = CID;
+            CheckOutDate = COD;
+        }
+        public AddRoom(Room r)
+        {
+            RoomList.Add(r);
+        }
+        public double CalculateTotal()
+        {
+
+        }
+    }
+}
